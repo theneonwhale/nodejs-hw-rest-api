be6b2a20a5bc3157c3276b89665111e48ae795d7
Andrii Kylymnyk <a.kylymnyk@gmail.com>
2021-02-21 15:35:18 +0200

    add routes and validation

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,24 +1,111 @@
-const express = require('express')
-const router = express.Router()
+const express = require('express');
+const router = express.Router();
+const Contacts = require('../../model/index');
+const validate = require('./validation');
 
 router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+  try {
+    const contacts = await Contacts.listContacts();
+
+    return res.json({ status: 'success', code: 200, data: { contacts } });
+  } catch (error) {
+    next(error);
+  }
+});
 
 router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+  try {
+    const contact = await Contacts.getContactById(req.params.contactId);
 
-router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+    if (contact) {
+      return res.json({
+        status: 'success',
+        code: 200,
+        message: 'Contact was found',
+        data: { contact },
+      });
+    } else {
+      return res
+        .status(404)
+        .json({ status: 'error', code: 404, message: 'Not found' });
+    }
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.post('/', validate.addContact, async (req, res, next) => {
+  try {
+    const contact = await Contacts.addContact(req.body);
+
+    if (contact) {
+      return res.status(201).json({
+        status: 'success',
+        code: 201,
+        message: 'Contact was created',
+        data: { contact },
+      });
+    } else {
+      return res.status(400).json({
+        status: 'error',
+        code: 400,
+        message: 'Contact already exists',
+      });
+    }
+  } catch (error) {
+    next(error);
+  }
+});
 
 router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+  try {
+    const contact = await Contacts.removeContact(req.params.contactId);
 
-router.patch('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+    if (contact) {
+      return res.json({
+        status: 'success',
+        code: 200,
+        message: 'Contact deleted',
+        data: { contact },
+      });
+    } else {
+      return res
+        .status(404)
+        .json({ status: 'error', code: 404, message: 'Not found' });
+    }
+  } catch (error) {
+    next(error);
+  }
+});
 
-module.exports = router
+router.patch('/:contactId', validate.updateContact, async (req, res, next) => {
+  try {
+    const contact = await Contacts.updateContact(
+      req.params.contactId,
+      req.body,
+    );
+
+    if (JSON.stringify(req.body) === '{}') {
+      return res
+        .status(400)
+        .json({ status: 'error', code: 400, message: 'Missing fields' });
+    }
+
+    if (contact) {
+      return res.json({
+        status: 'success',
+        code: 200,
+        message: 'Contact was updated',
+        data: { contact },
+      });
+    } else {
+      return res
+        .status(404)
+        .json({ status: 'error', code: 404, message: 'Not found' });
+    }
+  } catch (error) {
+    next(error);
+  }
+});
+
+module.exports = router;
---
+++ routes/api/validation.js
@@ -0,0 +1,41 @@
+const Joi = require('joi');
+
+const schemaAddContact = Joi.object({
+  name: Joi.string().min(2).max(50).required(),
+  email: Joi.string().email().required(),
+  phone: Joi.string()
+    .pattern(
+      /^\+?[(]?[0-9]{2,4}[)]?\s?-?[0-9]{2,3}-?[0-9]{1,3}-?[0-9]{1,3}-?[0-9]{1,3}$/,
+    )
+    .required(),
+});
+
+const schemaUpdateContact = Joi.object({
+  name: Joi.string().min(2).max(50).optional(),
+  email: Joi.string().email().optional(),
+  phone: Joi.string()
+    .pattern(
+      /^\+?[(]?[0-9]{2,4}[)]?\s?-?[0-9]{2,3}-?[0-9]{1,3}-?[0-9]{1,3}-?[0-9]{1,3}$/,
+    )
+    .optional(),
+});
+
+const validate = (schema, obj, next) => {
+  const { error } = schema.validate(obj);
+  if (error) {
+    const [{ message }] = error.details;
+    return next({
+      status: 400,
+      message: `Failed: ${message.replace(/"/g, '')}`,
+    });
+  }
+  next();
+};
+
+module.exports.addContact = (req, res, next) => {
+  return validate(schemaAddContact, req.body, next);
+};
+
+module.exports.updateContact = (req, res, next) => {
+  return validate(schemaUpdateContact, req.body, next);
+};
