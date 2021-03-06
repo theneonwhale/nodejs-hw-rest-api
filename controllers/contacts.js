3b97cdc617e9c7dbc56061865655a34fe4c3a0c7
Andrii Kylymnyk <a.kylymnyk@gmail.com>
2021-03-06 12:15:41 +0200

    hw-04: step 01 - adds users schema and model

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -1,4 +1,4 @@
-const Contacts = require('../model/index');
+const Contacts = require('../model/contacts');
 
 const get = async (req, res, next) => {
   try {
--- model/schemas/contact.js
+++ model/schemas/contact.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const { Schema, model } = mongoose;
+const { Schema, model, SchemaTypes } = mongoose;
 
 const contactSchema = new Schema(
   {
@@ -33,6 +33,10 @@ const contactSchema = new Schema(
     token: {
       type: String,
       default: '',
+    },
+    owner: {
+      type: SchemaTypes.ObjectId,
+      ref: 'user',
     },
   },
   {
