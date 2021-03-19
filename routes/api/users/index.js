812365c6c352724628e744eb7868860998f4b3a2
Andrii Kylymnyk <a.kylymnyk@gmail.com>
2021-03-19 13:39:23 +0200

    adds GET endpoint /auth/verify/:verificationToken

--- routes/api/users/index.js
+++ routes/api/users/index.js
@@ -22,5 +22,5 @@ router.patch(
   [guard, upload.single('avatar'), validate.uploadAvatar],
   userController.avatars,
 );
-
+router.get('/auth/verify/:verificationToken', userController.verify);
 module.exports = router;
